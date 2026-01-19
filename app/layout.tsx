67e82df2ce7bfa40848125dd56f8696f5fe9043f
Gustavo Ferreira <trogist15496@gmail.com>
2026-01-19 17:34:05 +0100

    Icon fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,7 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
