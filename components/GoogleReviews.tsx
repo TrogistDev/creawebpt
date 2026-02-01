52ae5189570c6430e01be5e2793d892f1a3a7307
Gustavo Ferreira <trogist15496@gmail.com>
2026-02-01 17:09:59 +0100

    google reviews added

--- components/GoogleReviews.tsx
+++ components/GoogleReviews.tsx
@@ -1,17 +1,26 @@
 "use client";
+import { useEffect } from "react";
 import Script from "next/script";
 
 export default function GoogleReviews() {
-  return (
-    <div className="w-full my-10 flex justify-center">
-      {/* O container onde o widget será renderizado */}
-      <div 
-        className="ti-widget" 
-        data-layout-id="cf9d922630e7734e1896a941589"
-        style={{ minHeight: '150px' }}
-      ></div>
+  useEffect(() => {
+    // Função para forçar o carregamento
+    const initTrustIndex = () => {
+      // @ts-ignore
+      if (window.Trustindex && window.Trustindex.init) {
+        // @ts-ignore
+        window.Trustindex.init();
+      }
+    };
 
-      {/* Script carregado diretamente no componente para garantir execução */}
+    // Tenta inicializar após um curto delay para garantir que o DOM está pronto
+    const timer = setTimeout(initTrustIndex, 500);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="w-full my-8 flex justify-center min-h-[150px]">
       <Script
         src="https://cdn.trustindex.io/loader.js?cf9d922630e7734e1896a941589"
         strategy="afterInteractive"
@@ -20,6 +29,10 @@ export default function GoogleReviews() {
           if (window.Trustindex) window.Trustindex.init();
         }}
       />
+      <div 
+        className="ti-widget" 
+        data-layout-id="cf9d922630e7734e1896a941589"
+      ></div>
     </div>
   );
 }
